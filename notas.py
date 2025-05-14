4fc46eb821d1926b160ab20645a0701f0ab854cf
tiaago17 <tiaago288@gmail.com>
2025-05-14 20:15:10 -0300

    Update notas.py
    Update notas.py

--- notas.py
+++ notas.py
@@ -6,15 +6,10 @@ for i in range(5):
     notadois = float(input("Digite sua nota na Avaliação 2: "))
     media = (notaum + notadois) / 2
 
-    notas.append({
-        "Nome": nome,
-        "Nota 1": notaum,
-        "Nota 2": notadois,
-        "Média": media
-    })
+    notas.append([nome, notaum, notadois, media])
 
 
 print ("\n ---- RESULTADO FINAL ----")
 
 for nota in notas:
-    print(f"{nota['Nome']} - Nota 1: {nota['Nota 1']} | Nota 2: {nota['Nota 2']} | Média: {nota['Média']:.2f}")
+    print(f"{nota[0]} - Nota 1: {nota[1]} | Nota 2: {nota[2]} | Média: {nota[3]:.2f}")
